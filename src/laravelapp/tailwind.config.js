29ae9bf54de92e53774b8fc26daae87867072328
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-02-03 06:58:26 +0900

    tailwindcss導入

---
+++ src/laravelapp/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    important: true,
+
+    purge: ['./src/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+
+    theme: {
+      extend: {}
+    },
+    variants: {},
+    plugins: []
+  }
--- src/laravelapp/webpack.mix.js
+++ src/laravelapp/webpack.mix.js
@@ -1,15 +1,33 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
+require('laravel-mix-purgecss');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
+mix
+.js('resources/js/app.js', 'public/js')
+// sass
+.sass('resources/sass/app.scss', 'public/css')
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+// tailwind
+.options({
+    processCssUrls: false,
+    postCss: [ tailwindcss('./tailwind.config.js') ],
+}).purgeCss()
+
+
+// ブラウザーリロード
+.browserSync({
+    proxy: {
+        target: "http://localhost:8000/",
+    },
+    files: [
+        './resources/**/*',
+        './public/**/*',
+    ],
+    open: false,
+    reloadOnRestart: true,
+})
+
+// キャッシュ削除
+if(mix.inProduction()){
+    mix.version()
+}
