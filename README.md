062f72c09ad495b6ccb217bbe2d5eb23e49a6c0a
Shun-ichiroTanaka <shun.ichiro.tanaka16@gmail.com>
2021-02-03 22:23:51 +0900

    docker環境を再構築

--- README.md
+++
@@ -1,9 +0,0 @@
-# Laravel-Vue-Taskapp
-
-- Docker 3.8
-- Apache 2.4.38
-- MySQL 5.7.3
-
-- PHP 7.2.5
-- Laravel 7.0
-- Vue.js 2.5.17
--- src/laravelapp/clear_cache.sh
+++
@@ -1,9 +0,0 @@
-php artisan cache:clear &&
-php artisan config:clear &&
-php artisan config:cache &&
-php artisan route:clear &&
-php artisan view:clear &&
-php artisan clear-compiled &&
-php artisan optimize &&
-composer dump-autoload &&
-rm -f bootstrap/cache/config.php
